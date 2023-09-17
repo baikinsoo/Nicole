b9e04d10b8072f00b247778678f4c2e663924ed8
baekinsoo <baikinsoo95@gmail.com>
2023-09-17 18:12:03 +0900

    nicole

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Super Converter</h1>
+      <label for="minutes">Minutes</label>
+      <input id="minutes" placeholder="Minutes" type="number" />
+      <label for="hours">Hours</label>
+      <input id="hours" placeholder="Hours" type="number" />
     </div>
   );
 }
