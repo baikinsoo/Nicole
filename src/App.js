d31620f6edd4c349e2cb47da991f9a8d2d1ccefa
baekinsoo <baikinsoo95@gmail.com>
2023-09-17 23:53:42 +0900

    0917 end

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [minutes, setMinutes] = useState(0);
+  const onChange = (event) => {
+    setMinutes(event.target.value);
+  };
   return (
     <div>
       <h1>Super Converter</h1>
       <label for="minutes">Minutes</label>
-      <input id="minutes" placeholder="Minutes" type="number" />
+      <input
+        value={minutes}
+        id="minutes"
+        placeholder="Minutes"
+        type="number"
+        onCahnge={onChange}
+      />
+      <h4>You want to convert {minutes}</h4>
       <label for="hours">Hours</label>
       <input id="hours" placeholder="Hours" type="number" />
     </div>
